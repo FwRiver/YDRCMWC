79d09f6114351658de766805b42ace4dbb5d798c
o0River0o <gyoushijie@gmail.com>
2021-05-31 20:14:16 -0700

    Updated

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -155,10 +155,6 @@ export default function Signup() {
                                     <option value="G3-A">G3-A</option>
                                     <option value="G4-A">G4-A</option>
                                     <option value="G4-B">G4-B</option>
-                                    <option value="G5">G5</option>
-                                    <option value="G6">G6</option>
-                                    <option value="G7">G7</option>
-                                    <option value="G8">G8</option>
                                 </Form.Control>
                             </Form.Group>
                         </Form.Row>   
